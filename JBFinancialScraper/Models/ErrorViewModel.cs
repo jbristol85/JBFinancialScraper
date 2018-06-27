6a74513889f9f7961bf69753f3ef8a31a2c8ce8f
jbristol85 <josh.bristol2@gmail.com>
2018-06-27 16:35:17 -0400

    Add project files.

---
+++ JBFinancialScraper/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace JBFinancialScraper.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
